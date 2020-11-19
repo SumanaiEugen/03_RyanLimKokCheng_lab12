a9e9f299d844a901b2806d93feaf4f9f9d44eff1
SumanaiEugen <ryanlim943@gmail.com>
2020-11-19 14:09:43 +0800

    Both in 1

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject PlayerGO;
+    Vector3 posoffset = new Vector3(0f,2f,-4f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, PlayerGO.transform.position + posoffset, 0.1f);
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public GameObject coinCollected;
+    bool touchenemy = false;
+    float coincount = 0f;
+    private float totalcoin;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalcoin = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        if (coincount == totalcoin)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+        if (transform.position.y < -5 || touchenemy == true)
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            
+                coincount++;
+                coinCollected.GetComponent<Text>().text = "Coin Collected: " + coincount;
+                Debug.Log(" 1 ");
+                Destroy(other.gameObject);
+            
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            touchenemy = true;
+        }
+    }
+}
